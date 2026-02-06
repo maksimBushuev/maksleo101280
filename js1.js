a36055a8fd1b98ee9596db339fa0bbea333796fd
maksimBushuev <46831404+maksimBushuev@users.noreply.github.com>
2026-02-06 23:13:20 +0500

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,40 +1,6 @@
-// 1. Инициализируем объект WebApp
-const tg = window.Telegram.WebApp;
+var WebApp = window.Telegram.WebApp;
 
-// 2. Сообщаем Telegram, что приложение готово (убирает лоадер)
-tg.ready();
-
-// 3. Получаем объект пользователя
-const user = tg.initDataUnsafe?.user;
-
-if (user) {
-    // Теперь у вас есть доступ к следующим полям:
-    const userData = {
-        id: user.id,                      // ID пользователя (число)
-        firstName: user.first_name,        // Имя
-        lastName: user.last_name || "",    // Фамилия (может быть пустой)
-        username: user.username || "",     // Никнейм @username
-        language: user.language_code,      // Язык (например, 'ru')
-        isPremium: user.is_premium         // Есть ли подписка Premium (true/false)
-    };
-
-    console.log("Данные получены:", userData);
-
-    // Пример: выводим имя пользователя на страницу
-    // Добавьте в HTML элемент <div id="user-name"></div>
-    const nameElement = document.getElementById('user-name');
-    if (nameElement) {
-        nameElement.innerText = `Привет, ${userData.firstName}!`;
-    }
-
-} else {
-    console.error("Сайт открыт не через Telegram или данные недоступны");
-}
-
-// Дополнительно: можно покрасить сайт в цвета темы пользователя
-document.body.style.backgroundColor = tg.themeParams.bg_color;
-document.body.style.color = tg.themeParams.text_color;
-
+WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
 
 
 
@@ -63,6 +29,7 @@ document.body.style.color = tg.themeParams.text_color;
 //         } else {
 //             alert("Библиотека загружена, но данных нет. Открой сайт через кнопку в боте!");
 //         }
+
 
 
 
